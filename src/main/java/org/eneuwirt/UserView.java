dfdbb606449734dafffcac133279927bcb0d350c
Eduard Neuwirt <eduard.neuwirt@googlemail.com>
2011-04-19 14:35:50 +0200

    roles and permissions implemented

--- src/main/java/org/eneuwirt/UserView.java
+++ src/main/java/org/eneuwirt/UserView.java
@@ -1,26 +1,54 @@
 package org.eneuwirt;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
-public class UserView  extends VerticalLayout
+public class UserView extends VerticalLayout
 {
 	private static final long serialVersionUID = 1L;
-    private MyVaadinApplication app;
+	private MyVaadinApplication app;
 
 
-    public UserView(MyVaadinApplication app)
+	public UserView(MyVaadinApplication app)
     {
         super();
         this.app = app;
 
-        Label label = new Label("Logged in");
-
+        Subject currentUser = SecurityUtils.getSubject();
+        Label label = new Label("Logged in as " +  currentUser.getPrincipal().toString());
+        
+        Button admin = new Button("For administrators only");
+        Button user  = new Button("For users only");
+        if (!currentUser.hasRole("admin"))
+        {
+        	admin.setEnabled(false);
+        } 
+        else if (!currentUser.hasRole("user"))
+        {
+        	user.setEnabled(false);
+        }
+        
+        
+        Button perm = new Button("For all with permission 'permission_2'");
+        if(!currentUser.isPermitted("permission_2"))
+        {
+        	perm.setEnabled(false);
+        }
+        
+        
         Button logout = new Button("logout");
         logout.addListener(new MyVaadinApplication.LogoutListener(this.app));
 
-        this.addComponent(logout);
         this.addComponent(label);
+        
+        this.addComponent(admin);
+        this.addComponent(user);
+        this.addComponent(perm);
+        this.addComponent(logout);
+    
     }
 }
